78f10c931c43ae7b9feabb08959922900fe020a4
Rafael Joseph Pagliuca dos Santos <rafael.santos.193@gmail.com>
2023-07-02 11:02:14 -0300

    feat:  Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,16 @@
 # Boot cluster from scratch:
 
-# One-liner
-minikube delete; minikube start; minikube addons enable ingress; for i in {1..3}; do kubectl apply -R -f manually-managed; sleep 3; done; minikube ip; while ! kubectl get secret -n argocd argocd-initial-admin-secret; do sleep 1; done;
+# Restart minikube
+minikube delete
+minikube start
+minikube addons enable ingress
+minikube addons enable ingress-dns
+
+# Apply manual resources
+for i in {1..3}; do
+    kubectl apply -R -f manually-managed;
+    sleep 10;
+done;
 
 # Setup minikube DNS
 sudo sh -c "echo resolv-file=/etc/resolv.dnsmasq.conf > /etc/dnsmasq.d/my-minikube"
@@ -11,5 +20,11 @@ sudo sh -c "echo nameserver 127.0.0.1 > /etc/resolv.conf"
 cat /etc/dnsmasq.d/my-minikube
 sudo service dnsmasq restart
 
+# Wait for ArgoCD server
+echo Waiting for ArgoCD server to be ready...
+kubectl wait --for=condition=ready pod -n argocd -l app.kubernetes.io/name=argocd-server
+
+# Wait until secret is ready
+# while ! kubectl get secret -n argocd argocd-initial-admin-secret; do sleep 1; done;
 echo ArgoCD app URL: http://argocd.my-minikube
 echo ArgoCD password:; ( kubectl get secret -n argocd argocd-initial-admin-secret -o json | jq .data.password -r | base64 -d ); echo;
